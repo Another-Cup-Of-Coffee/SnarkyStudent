294f141da96b39de4545da466661125669f4e016
Sydsthebest <224667762+Sydsthebest@users.noreply.github.com>
2026-03-28 13:08:45 -0500

    add scrape class

---
+++ src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/Scrape.java
@@ -0,0 +1,11 @@
+package net.anothercupofcoffee.snarkystudent.snarkystudent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Scrape {
+    
+}
