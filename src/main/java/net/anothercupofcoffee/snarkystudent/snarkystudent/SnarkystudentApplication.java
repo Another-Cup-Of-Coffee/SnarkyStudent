907fa566d7e463fc5bdc3821e5313145ef90ba5f
Ssolar5 <109261817+Ssolar5@users.noreply.github.com>
2026-03-28 11:24:04 -0500

    feat!: add files from <https://start.spring.io/>

---
+++ src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/SnarkystudentApplication.java
@@ -0,0 +1,13 @@
+package net.anothercupofcoffee.snarkystudent.snarkystudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnarkystudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnarkystudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/anothercupofcoffee/snarkystudent/snarkystudent/SnarkystudentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anothercupofcoffee.snarkystudent.snarkystudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnarkystudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
