67ab434b82eef2d71c6a558c91cb3ec62f6e4f2c
Ssolar5 <109261817+Ssolar5@users.noreply.github.com>
2026-03-28 14:26:56 -0500

    fix: import `Duration`

--- src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/Scrape.java
+++ src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/Scrape.java
@@ -1,5 +1,7 @@
 package net.anothercupofcoffee.snarkystudent.snarkystudent;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
