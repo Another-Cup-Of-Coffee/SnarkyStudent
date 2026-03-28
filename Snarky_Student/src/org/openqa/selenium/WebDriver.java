dffe47057e1b07b3426c2e6950b3ba9d64164ffe
ash-latsha <ashlatsha@gmail.com>
2026-03-28 13:02:43 -0500

    eclipse workspace in progress

---
+++ Snarky_Student/src/org/openqa/selenium/WebDriver.java
@@ -0,0 +1,14 @@
+package org.openqa.selenium;
+
+public class WebDriver {
+
+	public void get(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
