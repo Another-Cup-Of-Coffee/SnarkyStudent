f3d2e41e5c1640f835327dfe9e00c63dcb446e5e
Sydsthebest <224667762+Sydsthebest@users.noreply.github.com>
2026-03-28 13:56:04 -0500

    scrape code add

--- src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/Scrape.java
+++ src/main/java/net/anothercupofcoffee/snarkystudent/snarkystudent/Scrape.java
@@ -1,11 +1,22 @@
 package net.anothercupofcoffee.snarkystudent.snarkystudent;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.time.Duration;
-import java.util.List;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Scrape {
-    
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private int pageNumber;
+    private int resultsPerPage = 120;
+    private boolean nextIsEnabled = true;
+
+    public void registrationScraper() {
+        driver = new ChromeDriver();
+        driver.get("https://ssbprod.utsa.edu/StudentRegistrationSsb/ssb/classSearch/classSearch");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        
+    }
+
 }
